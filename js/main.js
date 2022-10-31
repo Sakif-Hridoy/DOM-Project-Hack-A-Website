eb19dd8616199f2ace716fa986b2756594a8edcb
sakif hridoy <silenticebreaker@gmail.com>
2022-10-31 14:49:18 +0600

    show inside elements of milestone with dom access elements methods

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function loadMilestones(){
         return `<div class="milestone border-b">
         <div class="flex">
           <div class="checkbox"><input type="checkbox" /></div>
-          <div>
+          <div onclick="openMilestone(this)">
             <p>
               ${milestone.name}
               <span><i class="fas fa-chevron-down"></i></span>
@@ -18,15 +18,24 @@ function loadMilestones(){
           </div>
         </div>
         <div class="hidden_panel">
-          <div class="module border-b">
-            <p>Module Name</p>
-          </div>
+          ${milestone.modules.map(function(module){
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+            </div>`;
+          }).join("")}
         </div>
       </div>`
 
     }).join('')
 
 }`;
+
+}
+
+function openMilestone(milestoneElement){
+  const currentPanel = milestoneElement.parentNode.nextElementSibling;
+
+  currentPanel.classList.toggle('show');
 
 }
 
